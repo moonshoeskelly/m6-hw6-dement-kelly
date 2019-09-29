c9cdcf9e7298540b5f618165c10f3beff461a1a2
Kelly DeMent <kdement@ufl.edu>
2019-09-29 02:54:52 -0700

    Create message on box one click

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var allBoxes = document.getElementsByTagName('p')
+//Highlight all boxes on hover
+var allBoxes = document.getElementsByTagName('p');
 
 function changeThis () {
 for (i=0; i<allBoxes.length; i++) {
@@ -9,3 +10,13 @@ for (i=0; i<allBoxes.length; i++) {
 for (i = 0; i<allBoxes.length; i++) {
   allBoxes[i].addEventListener('mouseover', changeThis);
 }
+
+//Message on box one click
+var addOne = document.createElement('h3');
+addOne.innerHTML = 'Oooh - so close, but no cigar';
+
+function addThis () {
+  allBoxes[0].appendChild(addOne);
+}
+
+allBoxes[0].addEventListener('click', addThis);
