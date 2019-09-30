af24cf8c46aeea5d346789571ed5f17e90724d25
Kelly DeMent <kdement@ufl.edu>
2019-09-29 17:16:40 -0700

    Reveal button on any box click

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ for (i = 0; i<allBoxes.length; i++) {
   allBoxes[i].addEventListener('mouseover', changeThis);
 }
 
+
+
 //Message on box one click
 var addOne = document.createElement('h3');
 addOne.innerHTML = 'Oooh - so close, but no cigar';
@@ -21,6 +23,8 @@ function addThis () {
 
 allBoxes[0].addEventListener('click', addThis);
 
+
+
 //Message on box two click
 var addTwo = document.createElement('h3');
 addTwo.innerHTML = 'DING DING DING - We have a winner';
@@ -30,3 +34,26 @@ function addThat () {
 }
 
 allBoxes[1].addEventListener('click', addThat);
+
+
+
+//Message on box three click
+var addThree = document.createElement('h3');
+addThree.innerHTML = 'Oops, butter luck next time';
+
+function addThem () {
+  allBoxes[2].appendChild(addThree);
+}
+
+allBoxes[2].addEventListener('click', addThem);
+
+
+//Add button that refreshes by removing hidden class
+function myButton() {
+  var button = document.getElementById("buttonbox");
+  button.classList.remove("hidden");
+}
+
+for (i = 0; i<allBoxes.length; i++) {
+  allBoxes[i].addEventListener('click', myButton);
+}
